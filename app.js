f56da2acb8b9dae272d6c18a10a2254d7d6e09da
Daniel González Briseño <daniel.gb@globant.com>
2022-01-24 17:25:25 -0600

    Window Object

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-let a = 1;
-let b = 2;
-let c = 3;
-let d = "Lorem";
-let e = true;
-
-console.table({ a, b, c, d, e });
-
-const result = a + b;
-
-const greeting = "Hello World";
-
-console.log( a + " + " + b + " = ", result );
-
-console.log( greeting );
+// This variable is available
+// on "Window Object"
+var firstName = "Daniel";
