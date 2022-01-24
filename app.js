a224d5cc3e9a90106261a0346a4a1820f1af2771
Daniel González Briseño <daniel.gb@globant.com>
2022-01-24 17:13:17 -0600

    Debugging

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-let a = 1, b = 2, c = 3, d = "Lorem", e = true;
+let a = 1;
+let b = 2;
+let c = 3;
+let d = "Lorem";
+let e = true;
 
 console.table({ a, b, c, d, e });
 
 const result = a + b;
 
+const greeting = "Hello World";
+
 console.log( a + " + " + b + " = ", result );
+
+console.log( greeting );
