5346034b52b61737d3ed35a4e2d8ce3ebed194b9
Daniel González Briseño <70177108+qbixmex@users.noreply.github.com>
2022-01-25 04:27:26 -0600

    Alert, Prompt, Confirm Updated

--- 00-essentials/04-alert_prompt_confirm.js
+++ 00-essentials/04-alert_prompt_confirm.js
@@ -9,10 +9,6 @@ console.log( message );
 /* --------- CONFIRM --------- */
 const selection = confirm("Are you sure ?");
 
-<<<<<<< HEAD
 const confirmMessage = ( selection ) ? true : false;
-=======
-const confirmMessage = ( confirm ) ? true : false;
->>>>>>> d8ff3ae66758aeb42be945ac4d7a60446db6366f
 
 console.log( "Your answer is: ", confirmMessage );
