e188a143f40edae3a56140b2c024a265ed4a8a01
Daniel González Briseño <daniel.gb@globant.com>
2022-02-09 15:54:14 -0600

    getters and setters

--- 04-classes/classes/02-classes.js
+++ 04-classes/classes/02-classes.js
@@ -1,2 +1,7 @@
 const book = new Book("Javascript", "beginner", 12.90);
+
+book.setAvailable = true;
+
+book.setQuantity = 20;
+
 book.print();
--- 04-classes/classes/Book.js
+++ 04-classes/classes/Book.js
@@ -6,7 +6,9 @@
  * book.printTotal();
  */
 class Book {
-  tax = 4;
+  #tax = 10;
+  #_quantity = 0;
+  available = false;
 
   /**
    * @param {string} title example: Javascript, Ruby, Python
@@ -24,17 +26,50 @@ class Book {
     let message = "";
     message += `Title: ${ this.title }\n`;
     message += `Level: ${ this.level }\n`;
-    message += `Tax: $${ this.calculateTax() } (${ this.tax }%)\n`;
-    message += `Price: $${ this.price.toFixed(2) }\n`;
-    message += `Total Price: $${ this.totalPrice().toFixed(2) }\n`;
+    message += `Tax: $${ this.calculateTax() } (${ this.#tax }%)\n`;
+    message += `Quantity: ${ this.quantity }\n`;
+    message += `Available: ${ this.available ? "Yes": "No" }\n`;
+    message += `Unit Price: $${ this.price.toFixed(2) }\n`;
+    message += `Total Price: $${ this.totalPrice(this.quantity).toFixed(2) }\n`;
     console.log( message );
   }
 
-  calculateTax() {
-    return (this.price * this.tax) / 100;
+  /**
+   * Set available status.
+   * 
+   * @param {boolean} value true | false
+   */
+  set setAvailable( value ) {
+    this.available = value;
   }
 
-  totalPrice() {
-    return this.price + this.calculateTax();
+  get getTax() {
+    return this.#tax;
+  }
+
+  get quantity() {
+    return this.#_quantity;
+  }
+
+  /**
+   * Set Quantity value
+   * @param {number} value
+   **/
+  set setQuantity( value ) {
+    this.#_quantity = value;
+  }
+
+  /**
+   * Get price with tax included.
+   * @returns {number} example: 1.00 to 1.15 if tax is 15%
+   */
+  calculateTax() {
+    return (this.price * this.getTax) / 100;
+  }
+
+  /** @param {number} quantity */
+  totalPrice( quantity ) {
+    if (quantity == 0) return 0;
+    return (this.price * quantity) + this.calculateTax();
   }
 }
